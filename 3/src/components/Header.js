77c96a8223492cdb7f6dda7d6f9cb291efc21c88
Stefan <stefankotarac01@gmail.com>
2022-06-22 08:21:30 +0200

    wip

--- 3/src/components/Header.js
+++ 3/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img src="./images/troll-face.png" />
-            <h2>Header component</h2>
+            <h2>Header component</h2> 
             <h4>React Course - Project 3</h4>
         </header>
     )
